c3c132c36b81da7ec4863d3638c4dd4e65a34d95
ilgan <2ilgan1601@gmail.com>
2024-06-13 18:47:30 +0300

    add models: AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Ticket, Reservation

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "AstronomyShows"
+
+    def __str__(self):
+        return f"AstronomyShow {self.title}, {self.description}"
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "ShowThemes"
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "PlanetariumDomes"
+
+    def __str__(self):
+        return f"PlanetariumDome {self.name}, rows: {self.rows}, seats in row: {self.seats_in_row}"
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
+    planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
+    show_time = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = "ShowSessions"
+
+    def __str__(self):
+        return f"ShowSession, astronomy show: {self.astronomy_show.title}, planetarium dome: {self.planetarium_dome.name}, show time: {self.show_time}"
+
+
+class Ticket(models.Model):
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE, null=True, related_name="ticket")
+
+    class Meta:
+        verbose_name_plural = "Tickets"
+
+    def __str__(self):
+        return f"Ticket. Seat: {self.seat}, show session: {self.show_session}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Reservations"
+
+    def __str__(self):
+        return f"Reservation. Created at: {self.created_at}, user: {self.user.first_name} {self.user.last_name}"
