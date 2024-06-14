c90c95c2aa41b13f791e08627cdc4676fb3c0f81
ilgan <2ilgan1601@gmail.com>
2024-06-14 17:10:52 +0300

    update views PlanetariumDomeList and PlanetariumDomeDetail using generics and mixins

--- planetarium/views.py
+++ planetarium/views.py
@@ -2,47 +2,36 @@ from planetarium.models import PlanetariumDome
 from planetarium.serializers import PlanetariumSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.generics import get_object_or_404
 
 
-class PlanetariumDomeList(APIView):
-    def get(self, request) -> Response:
-        planetarium_domes = PlanetariumDome.objects.all()
-        serializer = PlanetariumSerializer(planetarium_domes, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class PlanetariumDomeList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumSerializer
 
-    def post(self, request) -> Response:
-        serializer = PlanetariumSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs) -> Response:
+        return self.create(request, *args, **kwargs)
 
 
-class PlanetariumDomeDetail(APIView):
-    def get_object(self, pk) -> PlanetariumDome:
-        return get_object_or_404(PlanetariumDome, pk=pk)
+class PlanetariumDomeDetail(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
 
-    def get(self, request, pk) -> Response:
-        serializer = PlanetariumSerializer(self.get_object(pk))
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs) -> Response:
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk) -> Response:
-        serializer = PlanetariumSerializer(self.get_object(pk), data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(self.get_object(pk), status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs) -> Response:
+        return self.update(request, *args, **kwargs)
 
-    def patch(self, request, pk) -> Response:
-        serializer = PlanetariumSerializer(self.get_object(pk), data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(self.get_object(pk), status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def patch(self, request, *args, **kwargs) -> Response:
+        return self.partial_update(request, *args, **kwargs)
 
-    def delete(self, request, pk) -> Response:
-        planetarium = self.get_object(pk)
-        planetarium.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs) -> Response:
+        return self.destroy(*args, **kwargs)
