579ca67a0b8934b249745fddbe83c31c8ab557b8
ilgan <2ilgan1601@gmail.com>
2024-06-14 14:06:56 +0300

    update serializers.py: rename seas_in_rows to seats_in_row

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -6,7 +6,7 @@ class PlanetariumSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     rows = serializers.IntegerField(required=True)
-    seas_in_rows = serializers.IntegerField(required=True)
+    seats_in_row = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
         return PlanetariumDome.objects.create(**validated_data)
@@ -14,6 +14,6 @@ class PlanetariumSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.rows = validated_data.get('rows', instance.rows)
-        instance.seas_in_rows = validated_data.get('seas_in_rows', instance.seas_in_rows)
+        instance.seats_in_row = validated_data.get('seas_in_rows', instance.seats_in_row)
         instance.save()
         return instance
