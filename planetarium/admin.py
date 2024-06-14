b24f7fcacfbd6adc0a6c20027695f071a5f5ba30
ilgan <2ilgan1601@gmail.com>
2024-06-14 23:12:38 +0300

    feat: register AstronomyShow, ShowTheme, ShowSession, Ticket, Reservation in admin

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from planetarium.models import PlanetariumDome
+from planetarium.models import PlanetariumDome, AstronomyShow, ShowTheme, ShowSession, Ticket, Reservation
 
 admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowTheme)
+admin.site.register(ShowSession)
+admin.site.register(Ticket)
+admin.site.register(Reservation)
