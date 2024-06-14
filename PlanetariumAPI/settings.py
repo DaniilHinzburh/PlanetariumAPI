009ebdc308418e605697bb8a543c4c2c7cefe777
ilgan <2ilgan1601@gmail.com>
2024-06-14 15:18:09 +0300

    update settings.py

--- PlanetariumAPI/settings.py
+++ PlanetariumAPI/settings.py
@@ -44,6 +44,15 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'user.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
