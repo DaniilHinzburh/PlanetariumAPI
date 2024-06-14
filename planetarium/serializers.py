fcca1b9d2f40db160c57802cb9ff2db135c6f833
ilgan <2ilgan1601@gmail.com>
2024-06-14 20:35:00 +0300

    update serializers.py , using ModelSerializer

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -2,18 +2,7 @@ from rest_framework import serializers
 from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Ticket, Reservation
 
 
-class PlanetariumSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    rows = serializers.IntegerField(required=True)
-    seats_in_row = serializers.IntegerField(required=True)
-
-    def create(self, validated_data):
-        return PlanetariumDome.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.rows = validated_data.get('rows', instance.rows)
-        instance.seats_in_row = validated_data.get('seas_in_rows', instance.seats_in_row)
-        instance.save()
-        return instance
+class PlanetariumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanetariumDome
+        fields = ("id", "name", "rows", "seats_in_row")
