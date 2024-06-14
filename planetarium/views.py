b679bee1f6f137f97b4f204a2bc2be6c5135ea67
ilgan <2ilgan1601@gmail.com>
2024-06-14 17:22:30 +0300

    update views PlanetariumDomeList and PlanetariumDomeDetail using generics.ListCreateAPIView and generics.RetrieveUpdateDestroyAPIView

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,37 +1,13 @@
 from planetarium.models import PlanetariumDome
 from planetarium.serializers import PlanetariumSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status, generics, mixins
-from rest_framework.generics import get_object_or_404
+from rest_framework import generics
 
 
-class PlanetariumDomeList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class PlanetariumDomeList(generics.ListCreateAPIView):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumSerializer
 
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs) -> Response:
-        return self.create(request, *args, **kwargs)
-
-
-class PlanetariumDomeDetail(
-    generics.GenericAPIView,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin
-):
-
-    def get(self, request, *args, **kwargs) -> Response:
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs) -> Response:
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs) -> Response:
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs) -> Response:
-        return self.destroy(*args, **kwargs)
+class PlanetariumDomeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumSerializer
