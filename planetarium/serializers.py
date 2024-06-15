b2b8798b37fe2e9d01a97e1686c28b05989f04d1
ilgan <2ilgan1601@gmail.com>
2024-06-15 14:43:42 +0300

    feat: add AstronomyShowSerializer, ShowThemeSerializer, TicketSerializer, ReservationSerializer serializers

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -2,7 +2,31 @@ from rest_framework import serializers
 from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Ticket, Reservation
 
 
-class PlanetariumSerializer(serializers.ModelSerializer):
+class AstronomyShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "description", "show_themes")
+
+
+class ShowThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowTheme
+        fields = ("id", "name")
+
+
+class PlanetariumDomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanetariumDome
         fields = ("id", "name", "rows", "seats_in_row", "category")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "show_session", "reservation")
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ("id", "created_at", "user")
