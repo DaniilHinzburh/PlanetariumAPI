c48dd1be188d4e394ff79795068049709aba7d9f
ilgan <2ilgan1601@gmail.com>
2024-06-15 15:00:44 +0300

    feat: add AstronomyShowViewSet, ShowThemeViewSet, ShowSessionViewSet, TicketViewSet, ReservationViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,8 +1,34 @@
-from planetarium.models import PlanetariumDome
-from planetarium.serializers import PlanetariumSerializer
+from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Ticket, Reservation
+from planetarium.serializers import AstronomyShowSerializer, ShowThemeSerializer, PlanetariumDomeSerializer, \
+    ShowSessionSerializer, TicketSerializer, ReservationSerializer
 from rest_framework import viewsets
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
 
 
 class PlanetariumDomeViewSet(viewsets.ModelViewSet):
     queryset = PlanetariumDome.objects.all()
-    serializer_class = PlanetariumSerializer
+    serializer_class = PlanetariumDomeSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
