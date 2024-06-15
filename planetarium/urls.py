941965397db0364482dbbd3716fcff77d6c1a2e1
ilgan <2ilgan1601@gmail.com>
2024-06-15 15:01:10 +0300

    feat: add urls for AstronomyShowViewSet, ShowThemeViewSet, ShowSessionViewSet, TicketViewSet, ReservationViewSet

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
-from planetarium.views import PlanetariumDomeViewSet
+from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet, PlanetariumDomeViewSet, ShowSessionViewSet, \
+    TicketViewSet, ReservationViewSet
 from rest_framework import routers
 
 app_name = "planetarium"
 
 router = routers.DefaultRouter()
+router.register("astronomy_shows", AstronomyShowViewSet, basename="astronomy_shows_list")
+router.register("show_themes", ShowThemeViewSet, basename="show_themes_list")
 router.register("planetarium_domes", PlanetariumDomeViewSet, basename="planetarium_domes_list")
+router.register("show_sessions", ShowSessionViewSet, basename="show_sessions_list")
+router.register("tickets", TicketViewSet, basename="tickets_list")
+router.register("reservations", ReservationViewSet, basename="reservations_list")
 
 urlpatterns = router.urls
