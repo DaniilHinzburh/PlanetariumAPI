1cbeb622e82b3dcb1c421a0094304cf73dfb8ddb
ilgan <2ilgan1601@gmail.com>
2024-06-15 17:04:28 +0300

    feat: update TicketViewSet, ShowSessionViewSet,AstronomyShowViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,12 +1,24 @@
 from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Ticket, Reservation
 from planetarium.serializers import AstronomyShowSerializer, ShowThemeSerializer, PlanetariumDomeSerializer, \
-    ShowSessionSerializer, TicketSerializer, ReservationSerializer
+    ShowSessionSerializer, TicketSerializer, ReservationSerializer, ShowSessionListSerializer, TicketListSerializer, \
+    AstronomyShowListSerializer
 from rest_framework import viewsets
 
 
 class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.all()
     serializer_class = AstronomyShowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        return AstronomyShowSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            return queryset.prefetch_related("show_themes")
+        return queryset
 
 
 class ShowThemeViewSet(viewsets.ModelViewSet):
@@ -23,10 +35,32 @@ class ShowSessionViewSet(viewsets.ModelViewSet):
     queryset = ShowSession.objects.all()
     serializer_class = ShowSessionSerializer
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+        return ShowSessionSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            return queryset.select_related()
+        return queryset
+
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TicketListSerializer
+        return TicketSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            return queryset.select_related()
+        return queryset
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
