31b968bd3513d33cc75ab3238de1db48e39c12db
ilgan <2ilgan1601@gmail.com>
2024-06-15 13:01:02 +0300

    feat: add TicketInline and ReservationAdmin

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from planetarium.models import PlanetariumDome, AstronomyShow, ShowTheme, ShowSession, Ticket, Reservation
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(PlanetariumDome)
 admin.site.register(AstronomyShow)
 admin.site.register(ShowTheme)
 admin.site.register(ShowSession)
 admin.site.register(Ticket)
-admin.site.register(Reservation)
+admin.site.register(Reservation, ReservationAdmin)
