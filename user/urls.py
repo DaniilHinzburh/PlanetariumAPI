fdaf60ddf82681b3e8f37bd5fc71e671b035373f
ilgan <2ilgan1601@gmail.com>
2024-06-17 20:50:54 +0300

    feat: add manage user

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import CreateUserView, LoginUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 from rest_framework.authtoken import views
 
 app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register_user"),
-    path("login/", LoginUserView.as_view(), name="get_auth_token")
+    path("login/", LoginUserView.as_view(), name="get_auth_token"),
+    path("me/", ManageUserView.as_view(), name="manage.user")
 ]
