c7788a5c9d6f1453130ea467eb063b509ef35aad
ilgan <2ilgan1601@gmail.com>
2024-06-18 16:18:37 +0300

    feat: add permission class IsAuthenticatedReadOnly

--- planetarium/permissions.py
+++ planetarium/permissions.py
@@ -7,5 +7,13 @@ class IsAdminAll_or_IsAuthenticatedReadOnly(BasePermission):
         return bool(
             request.method in SAFE_METHODS and request.user and request.user.is_authenticated
         ) or (
-            request.user and request.user.is_staff
+                request.user and request.user.is_staff
+        )
+
+
+class IsAuthenticatedReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS and request.user and request.user.is_authenticated
         )
